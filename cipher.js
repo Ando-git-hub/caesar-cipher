67aa8f68489c1969b9c1f1f76020f2f3cec01869
Andranik Hakobyan <57172439+Ando-git-hub@users.noreply.github.com>
2022-09-25 01:30:33 +0400

    Create cipher.js

---
+++ cipher.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const fs = require('fs')
+const { cipher } = require('./utils')
+const inputFilePath = './input.txt';
+const outputFilePath = './output.txt';
+
+const key = Number(process.argv[2]);
+let text = process.argv[3];
+console.log('key ', key)
+console.log('type key ',typeof key)
+console.log('text ', text)
+
+if(!key) throw new Error('Key required');
+if(!Number.isInteger(key))  throw new Error('Key must be a number');
+
+if(!text) {
+    text = fs.readFileSync(inputFilePath).toString();
+    console.log('file content ', text)
+    if(!text) throw new Error('Nothing to encode');
+}
+
+fs.writeFileSync(outputFilePath, cipher(text, key))
+
+
+
+
